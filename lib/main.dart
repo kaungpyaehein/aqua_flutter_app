e41d7905b3d08e279d1dd088da5fb1b3ea621b3f
KP <kaungpyaeheinmtu@gmail.com>
2023-08-09 12:06:02 +0630

    Go Router and Auth Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:purifed_water_flutter/screens/auth.dart';
 import 'package:purifed_water_flutter/screens/chat_screen.dart';
+import 'package:purifed_water_flutter/screens/home_screen.dart';
 import 'package:purifed_water_flutter/screens/info_screen.dart';
 
 import 'firebase_options.dart';
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,12 +19,10 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  final _router = GoRouter(
-    initialLocation: '/',
-    routes: [
+  final _router = GoRouter(initialLocation: '/', routes: [
     StatefulShellRoute.indexedStack(
       branches: [
-         StatefulShellBranch(routes: [
+        StatefulShellBranch(routes: [
           GoRoute(
             path: "/chat",
             builder: (context, state) {
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
           GoRoute(
             path: '/',
             builder: (context, state) {
-              return const AuthPage();
+              return const HomeScreen();
             },
           )
         ]),
@@ -46,26 +46,35 @@ class MyApp extends StatelessWidget {
             },
           ),
         ]),
-         
       ],
       builder: (context, state, navigationShell) {
         return Scaffold(
-          body: navigationShell,
-          bottomNavigationBar: NavigationBar(
-            selectedIndex: navigationShell.currentIndex,
-            onDestinationSelected: (value) {
-              navigationShell.goBranch(value);
-              
+          body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Scaffold(
+                  body: navigationShell,
+                  bottomNavigationBar: NavigationBar(
+                      selectedIndex: navigationShell.currentIndex,
+                      onDestinationSelected: (value) {
+                        navigationShell.goBranch(value);
+                      },
+                      destinations: const [
+                        NavigationDestination(
+                            icon: Icon(Icons.chat_bubble_outline),
+                            label: "Chat"),
+                        NavigationDestination(
+                            icon: Icon(Icons.home), label: "Home"),
+                        NavigationDestination(
+                            icon: Icon(Icons.person), label: "Info"),
+                      ]),
+                );
+              } else {
+                return const LoginScreen();
+              }
             },
-            destinations: const [
-                            NavigationDestination(
-                icon: Icon(Icons.chat_bubble_outline), label: "Chat"),
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(
-                icon: Icon(Icons.person), label: "Info"),
-
-
-          ]),
+          ),
         );
       },
     ),
@@ -73,7 +82,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: _router,
     );
